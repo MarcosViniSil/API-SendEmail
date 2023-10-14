aed3add47ccd2520805063186d9664e100bb2cfe
MarcosViniSil <Marcossilv203@gmail.com>
2023-10-14 17:57:21 -0300

    create project

---
+++ src/test/kotlin/domain/com/email/email/application/example/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package domain.com.email.email.application.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
